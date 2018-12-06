ddc2182e5590dc913c66fdc71ecf8a0853f5c8a1
Muhammad Hafidz <hafidzamarul@gmail.com>
2018-12-06 18:16:21 +0700

    ini tadi habis demo

--- modal_content.php
+++ modal_content.php
@@ -1,21 +1,14 @@
 <?php 
-  function contentModal(){
-    echo "aas";
-  $dataDestinasi = "SELECT * FROM destinasi WHERE $selectedID";
+
+// include 'admin/conn.php';
+  $selectedID = 4;
+  $dataDestinasi = "SELECT * FROM destinasi WHERE ID_DESTINASI=$selectedID";
   $getData = mysqli_query(connection(),$dataDestinasi);
 
-  $data = mysqli_fetch_array($getData);
-  echo $data['ID_DESTINASI'];  
-  echo $data['NAMA'];  
-  echo $data['DESKRIPSI'];  
-  echo $data['HARGA'];  
-  echo $data['LOKASI'];  
-  echo $data['ALAMAT'].", ". $data['KOTA'].", ". $data['PROVINSI'];  
-  echo $data['GAMBAR'];  
-  echo $data['TANGGAL'];  
-  echo $data['RATE']; 
+  while($data = mysqli_fetch_array($getData)){
+  $array[]=$data;
   }
-
+  echo json_encode($array);
   // $selectedID ????
 
    
